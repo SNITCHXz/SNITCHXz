ad45641f7009803e0072a395fd6275dea20ff450
SNITCHXz <snitchmart44@gmail.com>
2025-06-05 00:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function orderWhatsApp(product) {
-  const phone = "9928678907";
-  const message = `Hi, I want to order ${product} from SNITCHX. Please assist me.`;
-  const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-  window.open(url, "_blank");
-}
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('Particles loaded.');
+});
